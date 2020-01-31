105fd85d6403ea3ff57e744fec62725ab37c7650
rupam v <rupampatel2006@gmail.com>
2020-01-31 09:31:13 -0500

    Add type for product and cleanup code

--- tds-popover/src/app/app.component.ts
+++ tds-popover/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {PopoverDataService} from './service/popover-data.service';
 import {cloneDeep} from 'lodash';
-import {Categories, CategoriesData} from './interface/categories';
+import {CategoriesData, Product} from './interface/categories';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +12,48 @@ import {Categories, CategoriesData} from './interface/categories';
 export class AppComponent implements OnInit {
   title = 'tds-popover';
   public categories: any = [];
-  public products: any;
+  public products: Product[];
 
   constructor(private popoverDataService: PopoverDataService) {
   }
 
   ngOnInit(): void {
+    this.prepareData();
+  }
+
+  selectCategory(item: any) {
+    item.checked = !item.checked;
+  }
+
+  done(item) {
+    item.products = this.products;
+    // Data can be written to backend as well
+  }
+
+  cancel() {
+  }
+
+  addProductQuantity(product: any) {
+    product.quantity++;
+  }
+
+  reduceProductQuantity(product: any) {
+    if (product.quantity > 0) {
+      product.quantity--;
+    }
+  }
+
+  totalQuantityOfEachProduct(item: any) {
+    const total = item.products.reduce((acc, product) => acc + product.quantity, 0);
+    return total || '';
+  }
+
+  getProductsCopy(products: any) {
+    this.products = cloneDeep(products);
+    return this.products;
+  }
+
+  private prepareData() {
     this.popoverDataService
       .getCategories()
       .subscribe((categories: CategoriesData) => {
@@ -35,37 +71,5 @@ export class AppComponent implements OnInit {
             };
           });
       });
-  }
-
-  check(item: any) {
-    item.checked = !item.checked;
-  }
-
-  done(item) {
-    item.products = this.products;
-    // Data can be written to backend as well
-  }
-
-  cancel() {
-  }
-
-  addQuantity(product: any) {
-    product.quantity++;
-  }
-
-  reduceQuantity(product: any) {
-    if (product.quantity > 0) {
-      product.quantity--;
-    }
-  }
-
-  totalQuantityOfEachProduct(item: any) {
-    const total = item.products.reduce((acc, product) => acc + product.quantity, 0);
-    return total || '';
-  }
-
-  getProductsCopy(products: any) {
-    this.products = cloneDeep(products);
-    return this.products;
   }
 }
--- tds-popover/src/app/interface/categories.ts
+++ tds-popover/src/app/interface/categories.ts
@@ -1,11 +1,22 @@
-  export interface Categories {
-    fruits: string[];
-    vegetables: string[];
-    beverages: string[];
-    snacks: string[];
-  }
+export interface Categories {
+  fruits: string[];
+  vegetables: string[];
+  beverages: string[];
+  snacks: string[];
+}
 
-  export interface CategoriesData {
-    categories: Categories;
-  }
+export interface CategoriesData {
+  categories: Categories;
+}
 
+
+export interface Product {
+  product: string;
+  quantity: number;
+}
+
+export interface Category {
+  key: string;
+  products: Product[];
+  checked: boolean;
+}
